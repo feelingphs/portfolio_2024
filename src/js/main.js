d09f8a6dbea58a0bf44ede3a0d6179b175d01937
sun <feelingphs@gmail.com>
2026-08-02 22:16:19 +0900

    2026 버전 작업중 / 상세페이지 추가

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     isActive();
+    layerOpen();
 })
 
 document.addEventListener('scroll', function() {
     // 애니메이션
     isActive();
+})
+
+document.addEventListener('click', function() {
+    
 })
 
 const windowHeight = window.innerHeight;
@@ -63,29 +68,42 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) || (/
 
 const section = document.querySelectorAll("section");
     
-section.forEach(function(item, i){
+// section.forEach(function(item, i){
 
-    if(i == 3) {
+//     if(i == 3) {
 
-    } else {
-        item.addEventListener("wheel", function (e) {
-            e.preventDefault();
+//     } else {
+//         item.addEventListener("wheel", function (e) {
+//             e.preventDefault();
     
-            let moveTop = window.scrollY;
+//             let moveTop = window.scrollY;
     
-            // 스크롤 내릴 때
-            if(e.deltaY > 0){
-                moveTop += section[(i+1)].getBoundingClientRect().top;
-            }
+//             // 스크롤 내릴 때
+//             if(e.deltaY > 0){
+//                 moveTop += section[(i+1)].getBoundingClientRect().top;
+//             }
         
-            // 스크롤 올릴 떄
-            if(e.deltaY < 0){
-                moveTop += section[(i-1)].getBoundingClientRect().top;
-            }
+//             // 스크롤 올릴 떄
+//             if(e.deltaY < 0){
+//                 moveTop += section[(i-1)].getBoundingClientRect().top;
+//             }
     
-            window.scrollTo({top: moveTop, left: 0, behavior: "smooth"});
-        })
-    }
+//             window.scrollTo({top: moveTop, left: 0, behavior: "smooth"});
+//         })
+//     }
 
 
-})
+// })
+
+// 상세페이지 레이어
+function layerOpen(){
+    const thumbList = document.querySelector('.works_list');
+    const thumbItem = document.querySelectorAll('.works_item')[0];
+
+    const layer = document.querySelector('.layer_detail');
+    const layerItem = document.querySelectorAll('.detail_item')[0];
+    thumbItem.querySelector('a').addEventListener('click', function() {
+        document.body.classList.add('scrollLock');
+        layerItem.classList.add('open');
+    })
+}
